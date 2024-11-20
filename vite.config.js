82810101fdd2fc5eee2e93d6034db88877cfe99e
Thaika <dummtmail@g.com>
2024-11-20 20:23:39 +0530

    added dist folder and vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+    base: "/insta_tailwind/"
+    // plugins: [
+    //     require("tailwindcss"),
+    //     require("autoprefixer")({
+    //         overrideBrowserslist: ["last 2 versions", "not dead"],
+    //         flexbox: "no-2009",
+    //     }),
+    // ],
+    // build: {
+    //     cssCodeSplit: true,
+    // },
+    // css: {
+    //     preprocessorOptions: {
+    //         tailwindcss: {
+    //             configPath: "tailwind.config.js",
+    //         },
+    //     },
+    // },
+})
